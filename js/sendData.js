3451d2f5e2ccee3f5853590425988c8eda73dceb
siparis515 <32108783+siparis515@users.noreply.github.com>
2021-11-11 13:32:21 +0300

    Add files via upload

--- js/sendData.js
+++ js/sendData.js
@@ -7,16 +7,16 @@ function sendData(){
 	localStorage.setItem('city', sessionStorage.getItem('city'));
 	localStorage.setItem('adress', sessionStorage.getItem('adress'));
 	sessionStorage.clear();
-	var url = "mailto:test@apple.com?subject=Interest+in+Steve's+life&amp;body=" +
-	"First+name:+" +
-	localStorage.getItem('firstName') + ",+Last+name:+" +
-	localStorage.getItem('lastName') + ",+" +
-	"E-mail:+" +
-	localStorage.getItem('email') + ",+Phone+number:+" +
-	localStorage.getItem('phoneNumber') + ",+" +
-	"Country:+" +
-	localStorage.getItem('country') + ",+City:+" +
-	localStorage.getItem('city') + ",+Adress:+" +
-	localStorage.getItem('adress');
+	var url = "mailto:test@apple.com&body=" +
+	"First name: " +
+	localStorage.getItem('firstName') + ", Last+name: " +
+	localStorage.getItem('lastName') + ", " +
+	"E-mail: " +
+	localStorage.getItem('email') + ", Phone number: " +
+	localStorage.getItem('phoneNumber') + ", " +
+	"Country: " +
+	localStorage.getItem('country') + ", City: " +
+	localStorage.getItem('city') + ", Adress: " +
+	localStorage.getItem('adress') + "?subject=Interest in Steve's life";
 	window.open(url, '_blank');
 }
